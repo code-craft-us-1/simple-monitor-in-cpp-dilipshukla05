4697bcf09926bd94079060035b0fd8d038163b8c
dilipshukla05 <dilip.shukla@philips.com>
2024-10-15 00:19:43 +0530

    2nd commit

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -5,17 +5,10 @@
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
   ASSERT_FALSE(vitalsOk(99, 102, 70));
   ASSERT_TRUE(vitalsOk(98.1, 70, 98));
-  
-  //Temp Checks
-  
-  ASSERT_FALSE(vitalsOk(104, 70, 98)); //Temp upper range  
-  ASSERT_TRUE(vitalsOk(93, 70, 98)); //Temp Lower range
-
-  //Pulse checks
-  ASSERT_FALSE(vitalsOk(93, 50, 98)); //Pulse Lower range
-  ASSERT_FALSE(vitalsOk(93, 110, 98)); //Pulse Lower range
-
-    //SPO2 checks
-  ASSERT_FALSE(vitalsOk(93, 70, 40)); //Pulse Lower range
-  ASSERT_TRUE(vitalsOk(93, 110, 112)); //Pulse Lower range
+  ASSERT_FALSE(vitalsOk(104, 70, 98)); // Temp upper range  
+  ASSERT_TRUE(vitalsOk(93, 70, 98)); // Temp Lower range  
+  ASSERT_FALSE(vitalsOk(93, 50, 98)); // Pulse Lower range
+  ASSERT_FALSE(vitalsOk(93, 110, 98)); // Pulse Lower range
+  ASSERT_FALSE(vitalsOk(93, 70, 40)); // Pulse Lower range
+  ASSERT_TRUE(vitalsOk(93, 110, 112)); // Pulse Lower range
 }

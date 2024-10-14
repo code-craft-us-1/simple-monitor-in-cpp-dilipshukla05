08e2f25e304a450033abdc40b0eeb960dbbd38bd
dilipshukla05 <dilip.shukla@philips.com>
2024-10-15 00:43:06 +0530

    Added More test case

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -3,12 +3,12 @@
 #include "gtest/gtest.h"
 
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-    ASSERT_FALSE(vitalsOk(99, 102, 70));
-    ASSERT_TRUE(vitalsOk(98.1, 70, 98));
-    ASSERT_FALSE(vitalsOk(104, 70, 98));  // Temp upper range
-    ASSERT_TRUE(vitalsOk(93, 70, 98));  // Temp Lower range
-    ASSERT_FALSE(vitalsOk(93, 50, 98));  // Pulse Lower range
-    ASSERT_FALSE(vitalsOk(93, 110, 98));  // Pulse Lower range
-    ASSERT_FALSE(vitalsOk(93, 70, 40));  // Pulse Lower range
-    ASSERT_TRUE(vitalsOk(93, 110, 112));  // Pulse Lower range
+    ASSERT_FALSE(vitalsOk(103, 105, 70));
+    ASSERT_TRUE(vitalsOk(98.1f, 70, 98));
+    ASSERT_FALSE(vitalsOk(99, 102, 70));  // Temp upper range
+    ASSERT_FALSE(vitalsOk(103, 80, 94));  // Temp Lower range
+    ASSERT_FALSE(vitalsOk(98, 105, 94));  // Pulse Lower range
+    ASSERT_FALSE(vitalsOk(98, 80, 70));  // Pulse Lower range
+    ASSERT_FALSE(vitalsOk(103, 105, 94));  // Pulse Lower range
+    ASSERT_FALSE(vitalsOk(103, 80, 70));  // Pulse Lower range
 }

26abcc1092d57dd0c8d551df179c4f0544a08714
dilipshukla05 <dilip.shukla@philips.com>
2024-10-15 00:13:16 +0530

    First chekin

--- monitor.cpp
+++ monitor.cpp
@@ -3,36 +3,42 @@
 #include <thread>
 #include <chrono>
 #include <iostream>
-using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
+#include <string>
+using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds, std::string;
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
+void GenerateWarning(string sAbNormalParameter) {
+    cout << sAbNormalParameter << " is out of range!\n";
     for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+        cout << "\r* " << flush;
+        sleep_for(seconds(1));
+        cout << "\r *" << flush;
+        sleep_for(seconds(1));
     }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  }
-  return 1;
 }
+bool IsTempInNormalRange(float temperature) {
+    if (temperature >= 102 || temperature <= 95) {
+        GenerateWarning("temperature");
+        return false;
+    }    
+    return true;
+}
+bool IsPulseInNormalRange(float pulseRate) {
+    if (pulseRate <= 60 || pulseRate >= 100) {
+        GenerateWarning("Pulse Rate");
+        return false;
+    }
+    return true;
+}
+
+bool IsSPO2InNormalRange(float spo2) {
+    if (spo2 <= 90) {
+        GenerateWarning("Oxygen Saturation");
+        return false;
+    }
+    return true;
+}
+
+
+bool vitalsOk(float temperature, float pulseRate, float spo2) {
+    return (IsTempInNormalRange(temperature) && IsPulseInNormalRange(pulseRate) && IsSPO2InNormalRange(spo2));
+    }
